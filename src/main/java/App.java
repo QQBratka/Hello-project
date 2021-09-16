0865b9cb00f9a0b47b5f318a76893ad1bc863949
Oleg Shiriaev <mystidreamer@gmail.com>
2021-09-17 02:06:48 +0300

    print method was changed

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,2 +1,6 @@
-package PACKAGE_NAME;public class App {
+public class App {
+    public static void main(String[] args) {
+        String mates = "mates!";
+        System.out.println("Hello " + mates);
+    }
 }
